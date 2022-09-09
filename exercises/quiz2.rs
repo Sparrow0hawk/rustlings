78eeb0befd9399d6a1ef9a2ca8ce171ee266efaa
Sparrow0hawk <alexjcoleman@gmail.com>
2022-09-09 16:05:53 +0100

    thinking like a rustaceans

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -30,11 +30,22 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
-            // TODO: Complete the function body. You can do it!
+            match command {
+                Command::Uppercase => output.push(string.to_uppercase()),
+                Command::Trim => output.push(string.trim().to_string()),
+                Command::Append(i) => {
+                    let mut new_string = string.to_owned();
+                    let bar: &str = "bar";
+
+                    new_string.push_str(&bar.repeat(*i));
+
+                    output.push(new_string)
+                }
+            }
         }
         output
     }
@@ -43,8 +54,8 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we have to import to have `transformer` in scope?
-    use ???;
     use super::Command;
+    use my_module::transformer;
 
     #[test]
     fn it_works() {
